85e318636ab14d997d4c8760411bbdb64a31593e
Nikolay Silantev <45269218+nsilantev91@users.noreply.github.com>
2021-09-22 17:01:17 +0300

    Added task 3 solution

---
+++ Task3/Task3/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackH = new System.Windows.Forms.TrackBar();
+            this.trackS = new System.Windows.Forms.TrackBar();
+            this.trackV = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackH
+            // 
+            this.trackH.Enabled = false;
+            this.trackH.Location = new System.Drawing.Point(482, 58);
+            this.trackH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackH.Maximum = 100;
+            this.trackH.Minimum = -100;
+            this.trackH.Name = "trackH";
+            this.trackH.Size = new System.Drawing.Size(155, 45);
+            this.trackH.TabIndex = 0;
+            this.trackH.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackS
+            // 
+            this.trackS.Enabled = false;
+            this.trackS.Location = new System.Drawing.Point(482, 204);
+            this.trackS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackS.Maximum = 100;
+            this.trackS.Minimum = -100;
+            this.trackS.Name = "trackS";
+            this.trackS.Size = new System.Drawing.Size(155, 45);
+            this.trackS.TabIndex = 1;
+            this.trackS.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackV
+            // 
+            this.trackV.Enabled = false;
+            this.trackV.Location = new System.Drawing.Point(482, 354);
+            this.trackV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackV.Maximum = 100;
+            this.trackV.Minimum = -100;
+            this.trackV.Name = "trackV";
+            this.trackV.Size = new System.Drawing.Size(155, 45);
+            this.trackV.TabIndex = 2;
+            this.trackV.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 40);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(452, 360);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(655, 40);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(451, 360);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 429);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Выбрать изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(902, 429);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 43);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(530, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Оттенок";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(518, 236);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Насыщенность";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 386);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Яркость";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 524);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trackV);
+            this.Controls.Add(this.trackS);
+            this.Controls.Add(this.trackH);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackH;
+        private System.Windows.Forms.TrackBar trackS;
+        private System.Windows.Forms.TrackBar trackV;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Task3/Task3/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap picture;
+        Bitmap changedPicture;
+        double currH = 0;
+        double currS = 0;
+        double currV = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                picture = new Bitmap(Image.FromFile(ofd.FileName), newSize: pictureBox1.Size);
+                pictureBox1.Image = picture;
+                changedPicture = (Bitmap)picture.Clone();
+                pictureBox2.Image = changedPicture;
+                trackH.Enabled = true;
+                trackS.Enabled = true;
+                trackV.Enabled = true;
+                button2.Enabled = true;
+            
+                trackH.Value = 0;
+                trackS.Value = 0;
+                trackV.Value = 0;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            currH = trackH.Value * 1.8;
+            Convert(currH, currS, currV);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            currS = trackS.Value / 100.0;
+            Convert(currH, currS, currV);
+        }
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            currV = trackV.Value / 100.0;
+            Convert(currH, currS, currV);
+        }
+        bool Equal(double x, double y)
+        { 
+           return Math.Abs(x - y) < 0.001;
+        }
+        
+        (double H, double S, double V) RGBtoHSV(double R, double G, double B)
+        {
+            double max = Math.Max(R, Math.Max(G, B)) * 1.0;
+            double min = Math.Min(R, Math.Min(G, B)) * 1.0;
+            double H = 0.0, S = 0.0, V = 0.0;
+            if (Equal(max, min))
+            {
+                H = 0.0;
+            }
+            else
+            {
+                if (Equal(max, R) && G >= B)
+                {
+                    H = 60.0 * ((G - B) / (max - min)) + 0.0;
+                }
+                else
+                if (Equal(max, R) && G < B)
+                {
+                    H = 60.0 * ((G - B) / (max - min)) + 360.0;
+                }
+                else
+                if (Equal(max, G))
+                {
+                    H = 60.0 * ((B - R) / (max - min)) + 120.0;
+                }
+                else
+                if (Equal(max, B))
+                {
+                    H = 60.0 * ((R - G) / (max - min)) + 240.0;
+                }
+            }
+            if (!Equal(max, 0.0))
+            {
+                S = 1.0 - (min / max);
+            }
+            V = max;
+            return (H, S, V);
+        }
+
+        (double R, double G, double B) HSVtoRGB(double H, double S, double V)
+        {
+            double Hi = Math.Floor(H / 60.0) % 6;
+            double f = H / 60.0 - Math.Floor(H / 60.0);
+            double p = V * (1 - S) * 1.0;
+            double q = V * (1 - f * S) * 1.0;
+            double t = V * (1 - (1 - f) * S) * 1.0;
+
+            if (Equal(Hi,0))
+            {
+                return (V, t, p);
+            }
+            if (Equal(Hi, 1))
+            {
+                return (q, V, p);
+            }
+            if (Equal(Hi, 2))
+            {
+                return (p, V, t);
+            }
+            if (Equal(Hi, 3))
+            {
+                return (p, q, V);
+            }
+            if (Equal(Hi, 4))
+            {
+                return (t, p, V);
+            }
+            if (Equal(Hi, 5))
+            {
+                return (V, p, q);
+            }
+            return (0, 0, 0);
+        }
+
+        void Convert(double H, double S, double V)
+        {
+            changedPicture = (Bitmap)picture.Clone();
+            for (int i = 0; i < changedPicture.Width; ++i)
+            {
+                for (int j = 0; j < changedPicture.Height; ++j)
+                {
+                    var pixel = changedPicture.GetPixel(i, j);
+                    var hsl = RGBtoHSV(pixel.R / 255.0, pixel.G / 255.0, pixel.B / 255.0);
+                    var newH = hsl.H;
+                    var newS = hsl.S;
+                    var newV = hsl.V;
+                    newH += H * 1.0;
+                    newS += S * 1.0;
+                    newV += V * 1.0;
+                    if (newH > 360)
+                        newH -= 360;
+                    if (newH < 0)
+                        newH += 360;
+                    newS = Math.Min(1, newS);
+                    newS = Math.Max(0, newS);
+                    newV = Math.Min(1, newV);
+                    newV = Math.Max(0, newV);
+                    var newRGB = HSVtoRGB(newH, newS, newV);
+                    changedPicture.SetPixel(i, j, Color.FromArgb(pixel.A, (int)(newRGB.R * 255.0), (int)(newRGB.G * 255.0), (int)(newRGB.B * 255.0)));
+                }
+            }
+            pictureBox2.Image = changedPicture;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            var path = "";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+            changedPicture.Save(path + "\\result.png");
+        }
+    }
+}
